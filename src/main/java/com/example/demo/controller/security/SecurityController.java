22e84e625a2d262fada49fdb8189ad04c42a0057
van <vanpersieyolo@gmail.com>
2020-10-30 08:22:58 +0700

    done layout homepage

--- src/main/java/com/example/demo/controller/security/SecurityController.java
+++ src/main/java/com/example/demo/controller/security/SecurityController.java
@@ -54,7 +54,7 @@ public class SecurityController {
     }
     @GetMapping("/homepage")
     public String homePage(){
-        return "homePage";
+        return "templateHomePage";
     }
     @GetMapping("/admin")
     public String adminPage(){
