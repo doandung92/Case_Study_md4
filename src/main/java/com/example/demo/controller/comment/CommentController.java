007c77bfd7bc16788d5d67e6ca59195e242f7886
van <vanpersieyolo@gmail.com>
2020-11-01 18:34:23 +0700

    loi redirect chua fix

--- src/main/java/com/example/demo/controller/comment/CommentController.java
+++ src/main/java/com/example/demo/controller/comment/CommentController.java
@@ -1,29 +1,57 @@
 package com.example.demo.controller.comment;
 
+import com.example.demo.model.article.Comment;
+import com.example.demo.model.article.Post;
+import com.example.demo.model.user.Users;
+import com.example.demo.service.commentService.ICommentService;
+import com.example.demo.service.post.IPostService;
+import com.example.demo.service.user.IUsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/comment")
 public class CommentController {
+    @Autowired
+    private IPostService postService;
+    @Autowired
+    private IUsersService usersService;
+    @Autowired
+    private ICommentService commentService;
+    private String getPrincipal() {
+
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails) principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
+    }
+
     @GetMapping("/commentPage/{id}")
     public String commentPage(@PathVariable Long id, Model model) {
-//        Post post = postService.findById(id).get();
+        Post post = postService.findById(id).get();
 //        Users mainUsers = usersService.findById(usersService.findByUsersName(getPrincipal()).getId()).get();
-//        Iterable<Comment> comments = commentService.findByPost(post); // findbyid
-//        model.addAttribute("mainUser",mainUsers);
-//        model.addAttribute("post", post);
-//        model.addAttribute("comment",new Comment());
+        Users mainUsers = usersService.findByUsersName(getPrincipal());
+        Iterable<Comment> comments = commentService.findByPost(post); // findbyid
+        model.addAttribute("mainUser",mainUsers);
+        model.addAttribute("post", post);
+        model.addAttribute("comment",new Comment());
 //        model.addAttribute("comments",comments);
         return "commentPage";
     }
     @PostMapping("/create")
-    public String createComment(){
-        System.out.println("-------");
-        return "homepage";
+    public String createComment(@ModelAttribute("comment") Comment comment){
+//            comment.setPost(post);
+            commentService.save(comment);
+        return "redirect:/homepage";
     }
 }
